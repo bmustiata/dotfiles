5118cb6ca8686756224bf401a28742a0c4df12e2
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-09-30 10:49:45 +0200

    Added some initial setup script.

---
+++ bin/initial-setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+export NVM_DIR="/home/bmustiata/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm install v6.7.0
+
+npm install -g project-archer
+npm install -g fast-live-reload
+
