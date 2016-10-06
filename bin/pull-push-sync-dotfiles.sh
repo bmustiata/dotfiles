9bf28ccbcfa53532d854ba05241a984da75f8659
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-10-06 18:09:38 +0200

    Submodules update.

--- bin/pull-push-sync-dotfiles.sh
+++ bin/pull-push-sync-dotfiles.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+set -e
+
 DOTFOLDER=$(readlink -f $(dirname $(readlink -f "$0"))/..)
 
 cd $DOTFOLDER
 git pull --rebase
+git submodule init
+git submodule update
 git push
 
 bin/sync-dotfiles.sh
