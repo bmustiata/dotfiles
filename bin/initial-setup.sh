e90158b0771cd8091d3a44eb0a785910320555d0
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2016-10-10 01:12:08 +0200

    Fixed home path

--- bin/initial-setup.sh
+++ bin/initial-setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
-export NVM_DIR="/home/bmustiata/.nvm"
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm install v6.7.0
 
