cd0d59fe58447b64463c8a11992b83c80a0d9fb8
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-01-21 23:03:31 +0100

    Destroy the old grid, create a new grid.

--- bin/germanium/run-grid.sh
+++ bin/germanium/run-grid.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+#
+# Destroy first the currently existing grid.
+#
+$(readlink -f $(dirname $(readlink -f "$0")))/destroy-grid.sh
+
+#
+# Create a new grid
+#
 docker run -d --name germanium-hub -p 4444:4444 germaniumhq/hub:1.10.4
 docker run -d --name germanium-node-chrome -e NODE_MAX_SESSION=10 -e NODE_MAX_INSTANCES=10 -p 15901:5901 --link germanium-hub:hub germaniumhq/node-chrome:1.10.4
 docker run -d --name germanium-node-firefox -e NODE_MAX_SESSION=10 -e NODE_MAX_INSTANCES=10 -p 15902:5901 --link germanium-hub:hub germaniumhq/node-firefox:1.10.4
