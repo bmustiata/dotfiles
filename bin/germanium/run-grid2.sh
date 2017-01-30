a75f02291292add68f497edef1c0109888e1d9f3
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-01-30 23:02:12 +0100

    sync

--- bin/germanium/run-grid2.sh
+++ bin/germanium/run-grid2.sh
@@ -8,7 +8,24 @@ $(readlink -f $(dirname $(readlink -f "$0")))/destroy-grid.sh
 #
 # Create a new grid
 #
-docker run -d --name germanium-hub -p 4444:4444 germaniumhq/hub
-docker run -d --name germanium-node-chrome -e NODE_MAX_SESSION=10 -e NODE_MAX_INSTANCES=10 -p 15901:5901 --link germanium-hub:hub germaniumhq/node-chrome
-docker run -d --name germanium-node-firefox -e NODE_MAX_SESSION=10 -e NODE_MAX_INSTANCES=10 -p 15902:5901 --link germanium-hub:hub germaniumhq/node-firefox
+docker run -d \
+    --name germanium-hub \
+    -p 4444:4444 \
+    germaniumhq/hub
+
+docker run -d \
+    --name germanium-node-chrome \
+    -e NODE_MAX_SESSION=10 \
+    -e NODE_MAX_INSTANCES=10 \
+    -p 15901:5901 \
+    --link germanium-hub:hub \
+    germaniumhq/node-chrome
+
+docker run -d \
+    --name germanium-node-firefox \
+    -e NODE_MAX_SESSION=10 \
+    -e NODE_MAX_INSTANCES=10 \
+    -p 15902:5901 \
+    --link germanium-hub:hub \
+    germaniumhq/node-firefox
 
