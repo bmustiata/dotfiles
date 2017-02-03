d62eeed81daf14ca4c956f528b502def429cf36b
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-02-03 16:22:20 +0100

    Update node, and install version-manager and eslint.

--- bin/initial-setup.sh
+++ bin/initial-setup.sh
@@ -3,10 +3,16 @@
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-nvm install v6.7.0
+nvm install v6
 
 npm install -g project-archer
 npm install -g fast-live-reload
+npm install -g version-manager
 
 # needed by the befautify commands in vim
 npm install -g js-beautify
+
+npm install -g eslint
+npm install -g eslint-plugin-import
+npm install -g eslint-config-airbnb-base
+
