c976386b5de83d6e39a6047e24269ae02a5cc2ea
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-05-11 23:47:35 +0200

    Updated the templates a bit.

--- bin/initial-setup.sh
+++ bin/initial-setup.sh
@@ -5,6 +5,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 nvm install v6
 
+npm install -g npm
+
 npm install -g project-archer
 npm install -g fast-live-reload
 npm install -g version-manager
