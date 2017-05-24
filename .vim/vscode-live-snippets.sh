d0fabcc3d65e15d3eac65ea947f88ebf7e8617ac
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-05-24 16:57:17 +0200

    Snippets editing is insta reloaded if writing them in the user folder.

---
+++ .vim/vscode-live-snippets.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cddot
+cd .vim
+flr -o . -e ./vscode-sync.sh
+
--- .vim/vscode-sync.sh
+++ .vim/vscode-sync.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-ultisnippets2vscode -t typescript.X_vue \
-    --out ${HOME}/programs/VSCode-linux-x64/resources/app/extensions/typescript/snippets/typescript.json
+ultisnippets2vscode -t X_ts.typescript \
+    --out ${HOME}/.config/Code/User/snippets/typescript.json
 ultisnippets2vscode -t X_tsx.X_ts.typescript \
-    --out ${HOME}/programs/VSCode-linux-x64/resources/app/extensions/typescript/snippets/typescriptreact.json
+    --out ${HOME}/.config/Code/User/snippets/typescriptreact.json
+
+# vue
+ultisnippets2vscode -t X_vue.X_css.X_xml.X_html.X_js_es2017.vue \
+    --out ${HOME}/.config/Code/User/snippets/vue.json
 
