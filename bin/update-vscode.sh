bd72f262fe9a88a12a298751cf18beccc855a8e1
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-05-26 17:09:37 +0200

    Added settings for gitlens (git blame utilities) VS Code plugin, foria TS snippet, and show the archive that is unpacked for vscode updates.

--- bin/update-vscode.sh
+++ bin/update-vscode.sh
@@ -2,8 +2,11 @@
 
 set -e
 
+ARCHIVE=$(readlink -f $(ls -t ~/Downloads/code-stable-code*.tar.gz | head -n 1))
+
 rm -fr ~/programs/VSCode-linux-x64
 cd ~/programs
-tar -zxvf $(readlink -f $(ls -t ~/Downloads/code-stable-code*.tar.gz | head -n 1))
+echo "Using: $ARCHIVE"
+tar -zxf $ARCHIVE
 #rm ~/Downloads/code-stable-code_*.tar.gz
 
