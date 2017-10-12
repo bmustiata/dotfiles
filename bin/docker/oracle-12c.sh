328a28e3090ab5b3fedad56a49674d2b3e2e5f7b
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-10-12 11:13:39 +0200

    Added oracle

---
+++ bin/docker/oracle-12c.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+docker run -d \
+    --name oracle-12c \
+    -v /opt/oracle-12c:/u01/app/oracle \
+    -p 1521:1521 \
+    -p 28080:8080 \
+    -e DBCA_TOTAL_MEMORY=1024 \
+    sath89/oracle-12c
+
