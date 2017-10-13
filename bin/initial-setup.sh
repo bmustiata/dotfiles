3b79b89d240fe6eb8392e307211c7059b3c4eef6
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-10-13 10:29:42 +0200

    oracle, incremental updates, new day with relative days

--- bin/initial-setup.sh
+++ bin/initial-setup.sh
@@ -1,26 +1,41 @@
 #!/usr/bin/env bash
 
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-nvm install v6
+TARGET_SYSTEM_VERSION="1"
+CURRENT_SYSTEM_VERSION=$(cat $HOME/.ciplogicdataversion)
 
-npm install -g npm
+#############################################################################
+# Do an incremental update of the packages and installed software on the
+# system.
+#############################################################################
+case "$CURRENT_SYSTEM_VERSION" in
+    "")
+        wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+        export NVM_DIR="$HOME/.nvm"
+        [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+        nvm install v6
 
-npm install -g project-archer
-npm install -g fast-live-reload
-npm install -g version-manager
+        npm install -g npm
 
-# for all the sweet projects in the .projects folder
-nmp install -g ars
+        npm install -g project-archer
+        npm install -g fast-live-reload
+        npm install -g version-manager
 
-# needed by the befautify commands in vim
-npm install -g js-beautify
+        # for all the sweet projects in the .projects folder
+        nmp install -g ars
 
-npm install -g eslint
-npm install -g eslint-plugin-import
-npm install -g eslint-config-airbnb-base
+        # needed by the befautify commands in vim
+        npm install -g js-beautify
 
-# static site generation
-npm install -g hexo
+        npm install -g eslint
+        npm install -g eslint-plugin-import
+        npm install -g eslint-config-airbnb-base
+
+        # static site generation
+        npm install -g hexo
+        ;;&
+    "0")
+        sudo apt install dateutils
+esac
+
+echo "$TARGET_SYSTEM_VERSION" > $HOME/.ciplogicdataversion
 
--- bin/new-day.sh
+++ bin/new-day.sh
@@ -2,7 +2,15 @@
 
 set -e
 
+DAY_FORMAT='%Y.%m.%d'
 mkdir -p $HOME/learn/learn/plan/daily/
-DAILY_FILE="$HOME/learn/learn/plan/daily/$(date '+%Y.%m.%d').adoc"
+
+if [[ $1 == "" ]]; then
+    COMPUTED_DAY=$(date "+$DAY_FORMAT")
+else # not [[ $1 == "" ]]
+    COMPUTED_DAY=$(dateutils.dadd -i $DAY_FORMAT $(date "+$DAY_FORMAT") -f $DAY_FORMAT ${1}d)
+fi   # else [[ $1 == "" ]]
+
+DAILY_FILE="$HOME/learn/learn/plan/daily/$COMPUTED_DAY.adoc"
 vim $DAILY_FILE
 
