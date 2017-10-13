4f1bf42bf4f0b23452e11be40d5f578d7247df46
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-10-13 10:44:19 +0200

    Allow specifying control days.

--- bin/new-day.sh
+++ bin/new-day.sh
@@ -3,14 +3,17 @@
 set -e
 
 DAY_FORMAT='%Y.%m.%d'
-mkdir -p $HOME/learn/learn/plan/daily/
+YEAR_FORMAT='%Y'
 
 if [[ $1 == "" ]]; then
     COMPUTED_DAY=$(date "+$DAY_FORMAT")
+    COMPUTED_YEAR=$(date "+$YEAR_FORMAT")
 else # not [[ $1 == "" ]]
     COMPUTED_DAY=$(dateutils.dadd -i $DAY_FORMAT $(date "+$DAY_FORMAT") -f $DAY_FORMAT ${1}d)
+    COMPUTED_YEAR=$(dateutils.dadd -i $DAY_FORMAT $(date "+$DAY_FORMAT") -f $YEAR_FORMAT ${1}d)
 fi   # else [[ $1 == "" ]]
 
-DAILY_FILE="$HOME/learn/learn/plan/daily/$COMPUTED_DAY.adoc"
+mkdir -p $HOME/learn/learn/plan/$COMPUTED_YEAR/daily/
+DAILY_FILE="$HOME/learn/learn/plan/$COMPUTED_YEAR/daily/$COMPUTED_DAY.adoc"
 vim $DAILY_FILE
 
--- bin/new-week.sh
+++ bin/new-week.sh
@@ -2,7 +2,19 @@
 
 set -e
 
-mkdir -p $HOME/learn/learn/plan/weekly/
-WEEKLY_FILE="$HOME/learn/learn/plan/weekly/week$(date '+%W').adoc"
+WEEK_FORMAT='%W'
+YEAR_FORMAT='%Y'
+DAY_FORMAT='%Y.%m.%d'
+
+if [[ $1 == "" ]]; then
+    COMPUTED_WEEK=$(date "+$WEEK_FORMAT")
+    COMPUTED_YEAR=$(date "+$YEAR_FORMAT")
+else # not [[ $1 == "" ]]
+    COMPUTED_WEEK=$(dateutils.dadd -i $DAY_FORMAT $(date "+$DAY_FORMAT") -f $WEEK_FORMAT ${1}w)
+    COMPUTED_YEAR=$(dateutils.dadd -i $DAY_FORMAT $(date "+$DAY_FORMAT") -f $YEAR_FORMAT ${1}w)
+fi   # else [[ $1 == "" ]]
+
+mkdir -p $HOME/learn/learn/plan/$COMPUTED_YEAR/weekly/
+WEEKLY_FILE="$HOME/learn/learn/plan/$COMPUTED_YEAR/weekly/week$COMPUTED_WEEK.adoc"
 vim $WEEKLY_FILE
 
