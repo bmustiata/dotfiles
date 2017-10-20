310ff4d530f6ee11cda1f3e69c07d740ca268b06
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-10-20 08:50:18 +0200

    Shell updates, new-day.sh to edit multiple days.

--- bin/new-day.sh
+++ bin/new-day.sh
@@ -5,15 +5,37 @@ set -e
 DAY_FORMAT='%Y.%m.%d'
 YEAR_FORMAT='%Y'
 
-if [[ $1 == "" ]]; then
-    COMPUTED_DAY=$(date "+$DAY_FORMAT")
-    COMPUTED_YEAR=$(date "+$YEAR_FORMAT")
-else # not [[ $1 == "" ]]
-    COMPUTED_DAY=$(dateutils.dadd -i $DAY_FORMAT $(date "+$DAY_FORMAT") -f $DAY_FORMAT ${1}d)
-    COMPUTED_YEAR=$(dateutils.dadd -i $DAY_FORMAT $(date "+$DAY_FORMAT") -f $YEAR_FORMAT ${1}d)
-fi   # else [[ $1 == "" ]]
+compute_file_name() {
+    #
+    # Computes the file name to edit for a daily view, starting
+    # from the index of the day. 0 is the current day, -1 is
+    # yesterday, 1 is tomorrow, etc.
+    #
+    if [[ $1 == "" ]]; then
+        computed_day=$(date "+$DAY_FORMAT")
+        computed_year=$(date "+$YEAR_FORMAT")
+    else # not [[ $1 == "" ]]
+        computed_day=$(dateutils.dadd -i $DAY_FORMAT $(date "+$DAY_FORMAT") -f $DAY_FORMAT ${1}d)
+        computed_year=$(dateutils.dadd -i $DAY_FORMAT $(date "+$DAY_FORMAT") -f $YEAR_FORMAT ${1}d)
+    fi   # else [[ $1 == "" ]]
 
-mkdir -p $HOME/learn/learn/plan/$COMPUTED_YEAR/daily/
-DAILY_FILE="$HOME/learn/learn/plan/$COMPUTED_YEAR/daily/$COMPUTED_DAY.adoc"
-vim $DAILY_FILE
+    echo "$HOME/learn/learn/plan/$computed_year/daily/$computed_day.adoc"
+}
+
+if [[ "$@" != "" ]]; then
+    days_from_user="$@"
+else # not [[ "$1" != "" ]]
+    days_from_user="0"
+fi   # else [[ "$1" != "" ]]
+
+
+IFS=', ' read -r -a days_planned <<< "$days_from_user"
+
+mkdir -p $HOME/learn/learn/plan/$computed_year/daily/
+
+for day in "${days_planned[@]}"; do
+    files_to_edit="$files_to_edit $(compute_file_name $day)"
+done
+
+vim -p $files_to_edit
 
