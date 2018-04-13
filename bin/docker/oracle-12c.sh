7a188d493e399e0e3e9ee47b4a18dabacf2d07ac
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-04-13 17:32:25 +0200

    Use other code page for oracle.

--- bin/docker/oracle-12c.sh
+++ bin/docker/oracle-12c.sh
@@ -5,7 +5,7 @@ docker run -d \
     -v /opt/oracle-12c:/u01/app/oracle \
     -p 1521:1521 \
     -p 28080:8080 \
-    -e CHARACTER_SET=WE8ISO8859P1 \
+    -e CHARACTER_SET=WE8MSWIN1252 \
     -e DBCA_TOTAL_MEMORY=1024 \
     sath89/oracle-12c
 
