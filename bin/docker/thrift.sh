d5701f1b6ecfd3fd92eb2ff9c7b2ea55a88f248a
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-06-19 16:30:20 +0200

    snippets.

--- bin/docker/thrift.sh
+++ bin/docker/thrift.sh
@@ -8,5 +8,6 @@ docker run -it \
     --rm \
     -v $CURRENT_DIR:$CURRENT_DIR \
     -u $(id -u):$(id -g) \
-    fbthrift-dev bash -c "cd $CURRENT_DIR; /usr/bin/thrift1 $ALL_PARAMETERS"
+    thrift:0.11 bash -c "cd $CURRENT_DIR; thrift $ALL_PARAMETERS"
+
 
