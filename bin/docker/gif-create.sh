31d9374f75199715477e315de40ddda073bf2ac0
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-10-12 06:37:49 +0200

    sync

--- bin/docker/gif-create.sh
+++ bin/docker/gif-create.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-VIDEO_FILE="$(readlink -f $1)"
-OUTPUT_FILE="$(readlink -f $2)"
+if [[ "$3" == "" ]]; then
+    SCALING_FACTOR="1.0"
+else # not [[ "$3" == "" ]]
+    SCALING_FACTOR="$3"
+fi   # else [[ "$3" == "" ]]
+
+echo "Scaling factor is: $3"
 
 docker run \
     --rm \
-    -v $VIDEO_FILE:/tmp/in/test.avi \
-    -v $(dirname $OUTPUT_FILE):/tmp/write \
-    -e OUTPUT_FILE_NAME=$(basename $OUTPUT_FILE) \
+    -v $1:/tmp/in/test.avi \
+    -v $(dirname $2):/tmp/write \
+    -e OUTPUT_FILE_NAME=$(basename $2) \
+    -e SCALING_FACTOR=$SCALING_FACTOR \
     bmst/gif-create
-
