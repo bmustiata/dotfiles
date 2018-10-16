fefe8e38ed7f1ce10a134807398112ffed8b173d
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-10-16 07:26:55 +0200

    sync

---
+++ bin/docker/vnc-server.sh
@@ -0,0 +1,10 @@
+docker run --name vnc-server \
+    --rm \
+    -d \
+    -e VNC_PASSWORD=germanium \
+    -e XFB_SCREEN=1080x600x24 \
+    -p 5901:5900 \
+    -p 6001:6000 \
+    bmst/x11server
+
+sleep 5 && docker exec -it vnc-server xhost +
