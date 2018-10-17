cc313634f9250887648ece72ce384e60b4e39d96
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-10-17 06:44:25 +0200

    sync

---
+++ bin/blog-photo.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if [[ "$2" == "" || "$1" == "" ]]; then
+    echo "You need to call: blog-photo.sh source_image.whatever output_image.png"
+    exit 1
+fi # [[ "$2" == "" || "$1" == "" ]]
+
+#convert $1 -trim +repage -resize '1280x720^' -gravity center -background transparent -extent 1280x720 $2
+convert $1 -resize '1280x720^' -gravity south -extent 1280x720 $2
+
