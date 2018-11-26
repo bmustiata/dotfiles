645faf1e611e0d509f484099499c120ec6f6e5a7
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-11-26 07:08:05 +0100

    sync

--- bin/blog-photo.sh
+++ bin/blog-photo.sh
@@ -7,4 +7,5 @@ fi # [[ "$2" == "" || "$1" == "" ]]
 
 #convert $1 -trim +repage -resize '1280x720^' -gravity center -background transparent -extent 1280x720 $2
 convert $1 -resize '1280x720^' -gravity south -extent 1280x720 $2
+convert $1 -resize '262x147^' -gravity south -extent 262x147 "jpg:${2}_small"
 
