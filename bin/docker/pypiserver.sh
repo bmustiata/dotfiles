714412fa43b960b058eae7a41d1159f64b40c774
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2018-11-29 12:57:05 +0100

    sync

--- bin/docker/pypiserver.sh
+++ bin/docker/pypiserver.sh
@@ -1,2 +1,9 @@
-docker run --name=nexus --volume="/home/raptor/tmp/pypi/_htaccess:/.htaccess:ro" --volume="/home/raptor/tmp/pypi/packages:/home/pypiserver/packages" --volume="/data/packages" -p 8081:8080 --restart=no pypiserver -P /.htaccess -v --overwrite
+docker run \
+    --name=nexus \
+    --volume="/home/raptor/tmp/pypi/_htaccess:/.htaccess:ro" \
+    --volume="/home/raptor/tmp/pypi/packages:/home/pypiserver/packages" \
+    --volume="/data/packages" \
+    -p 8081:8080 \
+    --restart=no \
+    pypiserver -P /.htaccess -v --overwrite
 
---
+++ bin/update-felix.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cd /tmp
+wget http://get.germaniumhq.com/felixbm/felixbm
+mv felixbm ~/bin/felixbm
+chmod +x ~/bin/felixbm
