86fe3714a37bbd4c05ab51dc97920878684bb26c
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2019-07-12 07:15:44 +0200

    sync

--- .projects/ts2-client/webpack.config.js
+++ .projects/ts2-client/webpack.config.js
@@ -1,51 +1,22 @@
-var fs = require("fs")
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: ['babel-polyfill', './src/ts/main.ts'],
-  output: {
-    filename: 'out/public/js/client.js',
-  },
-  resolve: {
-    extensions: ['', '.ts', '.tsx', '.js']
-  },
+  entry: './src/ts/main.ts',
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
-        }
-      },
-      {
-        test: function(name) {
-          console.log(name);
-
-          if (/\.tsx?$/.test(name)) {
-            return true;
-          }
-
-          if (!/\./.test(name)) {
-            return true;
-          }
-
-          return false;
-        },
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader?presets=es2015!ts-loader',
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
       }
     ]
   },
-  plugins: [
-    new CopyWebpackPlugin([
-    {
-        from: 'src/html',
-        to: 'out/public'
-    }
-    ])
-  ],
-  devtool: 'source-map'
-}
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
 
