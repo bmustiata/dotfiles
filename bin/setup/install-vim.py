6006e6f87592e9115a2c5931be4313ad0c51a946
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2019-08-30 17:07:44 +0200

    Install vim

---
+++ bin/setup/install-vim.py
@@ -0,0 +1,37 @@
+import adhesive
+
+
+@adhesive.task('Install\ vim\ package')
+def install_vim_package(context):
+    pass
+
+
+@adhesive.task('Vundle\ init')
+def vundle_init(context):
+    context.workspace.run("""
+        if [ ! -d $HOME/.vim/bundle/Vundle.vim ]; then
+            cd $HOME/.vim/
+            ./vundleinit.sh
+        fi
+    """)
+
+
+@adhesive.task('Install\ vundle\ packages')
+def install_vundle_packages(context):
+    context.workspace.run('vim "+silent! PluginInstall" +qall')
+
+
+@adhesive.task('YouCompleteMe\ Post\ Install')
+def youcompleteme_post_install(context):
+    context.workspace.run("""
+        cd $HOME/.vim/bundle/YouCompleteMe
+        ./install.sh
+    """)
+
+
+@adhesive.task('Install\ fonts')
+def install_fonts(context):
+    pass
+
+
+adhesive.bpmn_build("install-vim.bpmn")
