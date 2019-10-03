5bbc4a0cbec2505278b3e45f767f335439f35f60
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2019-10-03 06:57:13 +0200

    sync

--- bin/docker/asciidoctor.sh
+++ bin/docker/asciidoctor.sh
@@ -6,6 +6,7 @@ docker run --rm \
     -v $PROJECT_FOLDER:/documents \
     -v /etc/passwd:/etc/passwd:ro \
     -v /etc/group:/etc/group:ro \
+    -w "/documents" \
     -u $(id -u):$(id -g) \
     bmst/docker-asciidoctor \
     "/usr/bin/asciidoctor" "-r" "asciidoctor-diagram" "$@"
--- bin/docker/gif-create.sh
+++ bin/docker/gif-create.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+
+print_usage() {
+cat << EOM
+Usage: gif-create.sh input-file output-gif-name [scaling-factor]
+
+Scaling factor is implicitly 1.0
+EOM
+}
+
+if [[ "$2" == "" ]]; then
+    print_usage
+    exit 1
+fi # [[ "$2" == "" ]]
+
 if [[ "$3" == "" ]]; then
     SCALING_FACTOR="1.0"
 else # not [[ "$3" == "" ]]
@@ -10,8 +24,8 @@ echo "Scaling factor is: $3"
 
 docker run \
     --rm \
-    -v $1:/tmp/in/test.avi \
-    -v $(dirname $2):/tmp/write \
+    -v $(readlink -f $1):/tmp/in/test.avi \
+    -v $(readlink -f $(dirname $2)):/tmp/write \
     -e OUTPUT_FILE_NAME=$(basename $2) \
     -e SCALING_FACTOR=$SCALING_FACTOR \
     bmst/gif-create
