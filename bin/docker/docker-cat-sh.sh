8ad83e6803f1cfde3a137ec57b07e262bc683e0c
Mustiata, Bogdan <Bogdan.Mustiata@broadcom.com>
2020-01-27 17:22:12 +0100

    sync

--- bin/docker/docker-cat-sh.sh
+++ bin/docker/docker-cat-sh.sh
@@ -4,8 +4,9 @@ CONTAINER_ID=$(docker run -t --rm -d --entrypoint /bin/cat $1)
 if [[ "$2" == "" ]]; then
     CONTAINER_COMMAND=/bin/bash
 else # not [[ "$2" == "" ]]
-    CONTAINER_COMMAND=$2
+    shift  # we remove the container name
+    CONTAINER_COMMAND=$@
 fi   # else [[ "$2" == "" ]]
 
-docker exec -it $CONTAINER_ID $CONTAINER_COMMAND
+docker exec -it $CONTAINER_ID /bin/sh -c "$CONTAINER_COMMAND"
 docker rm -f $CONTAINER_ID
