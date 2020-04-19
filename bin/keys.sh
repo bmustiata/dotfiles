c12bf3bad8c9c67d085cf58b17d2daf673d57380
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2020-04-20 00:49:11 +0200

    sync

--- bin/keys.sh
+++ bin/keys.sh
@@ -1,23 +1,34 @@
 #!/usr/bin/env bash
 
-if [[ "$1" == "co" ]]; then
-    setxkbmap us -variant colemak -print | xkbcomp - $DISPLAY
-    echo "Reset the keys as colemak"
+yad=$(which yad)
+
+setkeyboard() {
+    if [[ "$1" == "co" ]]; then
+        setxkbmap us -variant colemak -print | xkbcomp - $DISPLAY
+        echo "Reset the keys as colemak"
+        exit 0
+    fi # [[ "$1" == "co" ]]
+
+    if [[ "$1" == "ro" ]]; then
+        setxkbmap ro -variant std -print | xkbcomp - $DISPLAY
+        echo "Reset the keys as Romanian"
+        exit 0
+    fi # [[ "$1" == "ro" ]]
+
+    if [[ "$1" == "de" ]]; then
+        setxkbmap de -print | xkbcomp - $DISPLAY
+        echo "Reset the keys as German"
+        exit 0
+    fi # [[ "$1" == "de" ]]
+
+    setxkbmap us -print | xkbcomp - $DISPLAY
+    echo "Reset the keys as US standard"
+}
+
+if [[ "$yad" != "" ]]; then
+    KEYBOARD=$($yad --entry co ro de us)
+    setkeyboard $KEYBOARD
     exit 0
-fi # [[ "$1" == "co" ]]
+fi # [[ "$yad" != "" ]]
 
-if [[ "$1" == "ro" ]]; then
-    setxkbmap ro -variant std -print | xkbcomp - $DISPLAY
-    echo "Reset the keys as Romanian"
-    exit 0
-fi # [[ "$1" == "ro" ]]
-
-if [[ "$1" == "de" ]]; then
-    setxkbmap de -print | xkbcomp - $DISPLAY
-    echo "Reset the keys as German"
-    exit 0
-fi # [[ "$1" == "de" ]]
-
-setxkbmap us -print | xkbcomp - $DISPLAY
-echo "Reset the keys as US standard"
-
+setkeyboard $1
