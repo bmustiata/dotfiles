136ae8e0dc6b7d220430ee4d038bdb73eb5d3bd1
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2020-05-12 02:08:46 +0200

    sync

--- bin/docker/asciidoctor-epub.sh
+++ bin/docker/asciidoctor-epub.sh
@@ -2,12 +2,16 @@
 
 PROJECT_FOLDER=$(pwd)
 
+if [[ -z "$ASCIIDOCTOR_DOCKER_IMAGE" ]]; then
+  ASCIIDOCTOR_DOCKER_IMAGE="bmst/docker-asciidoctor"
+fi # [[ -z "$ASCIIDOCTOR_DOCKER_IMAGE" ]]
+
 docker run --rm \
     -v $PROJECT_FOLDER:/documents \
     -v /etc/passwd:/etc/passwd:ro \
     -v /etc/group:/etc/group:ro \
     -w /documents \
     -u $(id -u):$(id -g) \
-    bmst/docker-asciidoctor \
+    $ASCIIDOCTOR_DOCKER_IMAGE \
     "/usr/bin/asciidoctor-epub3" "--trace" "-r" "asciidoctor-diagram" "$@"
 
