3f771c11d8f691da09d8f0c16bc9b0501e7f13d9
Mustiata, Bogdan <Bogdan.Mustiata@broadcom.com>
2020-06-18 12:59:50 +0200

    sync

--- bin/kubens.bash.complete.sh
+++ bin/kubens.bash.complete.sh
@@ -7,3 +7,42 @@ __kubens_complete() {
 
 complete -F __kubens_complete kubens
 
+#############################################################################
+# kubectl completion
+#############################################################################
+__kubectl_get_available_objects() {
+    OBJECT_TYPE="$2"
+
+    case "$OBJECT_TYPE" in
+        "edit"|"delete"|"apply"|"create"|"describe")
+            kubectl api-resources --no-headers | cut -f1 -d\ 
+        ;;
+        "logs")
+            OBJECT_TYPE="pods"
+            kubectl get --no-headers $OBJECT_TYPE | tr -s " " | perl -pe 's/(.*?)\s+(.*?)/\1  \2/'
+            kubectl get $OBJECT_TYPE --no-headers --all-namespaces | tr -s " " | perl -pe 's/(.*?)\s+(.*?)\s+(.*)/\1\/\2  \3/'
+        ;;
+        *)
+            kubectl get --no-headers $OBJECT_TYPE | tr -s " " | perl -pe 's/(.*?)\s+(.*?)/\1  \2/'
+            kubectl get $OBJECT_TYPE --no-headers --all-namespaces | tr -s " " | perl -pe 's/(.*?)\s+(.*?)\s+(.*)/\1\/\2  \3/'
+        ;;
+    esac
+
+}
+
+__start_kubectl() {
+    OBJECT_NAME=$(__kubectl_get_available_objects $@ | fzf | cut -f1 -d\ )
+    OBJECT_TYPE="$2"
+
+    if [[ "$OBJECT_NAME" = *"/"* ]]; then
+        IFS='/' read -r -a TOKENS <<< "$OBJECT_NAME"
+        RESULT="$OBJECT_TYPE --namespace ${TOKENS[0]} ${TOKENS[1]}"
+    else # not [[ "$OBJECT_NAME" = *"/"* ]]
+        RESULT="$OBJECT_TYPE $OBJECT_NAME"
+    fi   # else [[ "$OBJECT_NAME" = *"/"* ]]
+
+    COMPREPLY="$RESULT"
+}
+
+complete -F __start_kubectl kubectl
+
