2617af3e19ba1228a8c6ddfbbe529d2a94d89c39
Mustiata, Bogdan <Bogdan.Mustiata@broadcom.com>
2020-09-07 10:34:48 +0200

    sync

---
+++ bin/cert-dump.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+# echo | openssl s_client -showcerts -servername gnupg.org -connect gnupg.org:443 2>/dev/null | openssl x509 -inform pem -noout -text
+
+if [[ "$1" == "" ]]; then
+    echo "You need to pass the site name to dump the certificate from:"
+    echo "ie: $0 germaniumhq.com"
+    exit 1
+fi # [[ "$1" == "" ]]
+
+mkdir -p /usr/share/ca-certificates/extra
+echo | openssl s_client -showcerts -servername $1 -connect $1:443 > /usr/share/ca-certificates/extra/$1.crt
+sudo update-ca-certificates
+
