23c89b814879569c1f20362a35aa6dbdb1854091
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2020-11-21 08:26:18 +0100

    sync

--- bin/docker/asciidoctor.sh
+++ bin/docker/asciidoctor.sh
@@ -2,11 +2,15 @@
 
 PROJECT_FOLDER=$(pwd)
 
+if [[ -z "$ASCIIDOCTOR_SITE_FOLDER" ]]; then
+    ASCIIDOCTOR_SITE_FOLDER="/documents"
+fi # [[ -z "$ASCIIDOCTOR_SITE_FOLDER" ]]
+
 docker run --rm \
-    -v $PROJECT_FOLDER:/documents \
+    -v $PROJECT_FOLDER:$ASCIIDOCTOR_SITE_FOLDER \
     -v /etc/passwd:/etc/passwd:ro \
     -v /etc/group:/etc/group:ro \
-    -w "/documents" \
+    -w "$ASCIIDOCTOR_SITE_FOLDER" \
     -u $(id -u):$(id -g) \
     bmst/docker-asciidoctor \
     "/usr/bin/asciidoctor" "-r" "asciidoctor-diagram" "$@"
