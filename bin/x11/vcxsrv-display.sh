376534c455722d3b038345a1ebaa387549458cd5
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2021-02-19 16:59:44 +0100

    sync

--- bin/x11/vcxsrv-display.sh
+++ bin/x11/vcxsrv-display.sh
@@ -1,5 +1,14 @@
 
 echo Make sure you source this.
-export DISPLAY=$(awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null):0
+
+LOCAL_SERVER_IP=$(awk '/nameserver / {print $2; exit}' /etc/resolv.conf 2>/dev/null)
+
+export DISPLAY=$LOCAL_SERVER_IP:0
 export LIBGL_ALWAYS_INDIRECT=1
+
+echo "Run this in a command prompt"
+echo "set HOME=c:\\Users\\bm669313\\AppData\\Roaming"
+echo "type nul > c:\\Users\\bm669313\\AppData\\Roaming\\.Xauthority"
+echo "xauth add ${LOCAL_SERVER_IP}:0 . $(mcookie)"
+
 
