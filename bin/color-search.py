5bb01da532cf6d479622897709aaa5600e2e1a12
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2021-05-20 09:21:23 +0200

    sync

--- bin/color-search.py
+++ bin/color-search.py
@@ -17,7 +17,7 @@ __copyright__ = 'Copyright (C) 2011 Micah Elliott.  All rights reserved.'
 __license__   = 'WTFPL http://sam.zoy.org/wtfpl/'
 
 #---------------------------------------------------------------------
-
+import math
 import sys, re
 
 CLUT = [  # color look-up table
@@ -288,24 +288,10 @@ CLUT = [  # color look-up table
     ('255', 'eeeeee'),
 ]
 
-def _str2hex(hexstr):
-    return int(hexstr, 16)
-
-def _strip_hash(rgb):
-    # Strip leading `#` if exists.
-    if rgb.startswith('#'):
-        rgb = rgb.lstrip('#')
-    return rgb
-
-def _create_dicts():
-    short2rgb_dict = dict(CLUT)
-    rgb2short_dict = {}
-    for k, v in short2rgb_dict.items():
-        rgb2short_dict[v] = k
-    return rgb2short_dict, short2rgb_dict
 
 def short2rgb(short):
     return SHORT2RGB_DICT[short]
+
 
 def print_all():
     """ Print all 256 xterm color codes.
@@ -318,41 +304,36 @@ def print_all():
     print("Printed all codes.")
     print("You can translate a hex or 0-255 code by providing an argument.")
 
-def rgb2short(rgb):
-    """ Find the closest xterm-256 approximation to the given RGB value.
-    @param rgb: Hex code representing an RGB value, eg, 'abcdef'
-    @returns: String between 0 and 255, compatible with xterm.
-    >>> rgb2short('123456')
-    ('23', '005f5f')
-    >>> rgb2short('ffffff')
-    ('231', 'ffffff')
-    >>> rgb2short('0DADD6') # vimeo logo
-    ('38', '00afd7')
-    """
-    rgb = _strip_hash(rgb)
-    incs = (0x00, 0x5f, 0x87, 0xaf, 0xd7, 0xff)
-    # Break 6-char RGB code into 3 integer vals.
-    parts = [ int(h, 16) for h in re.split(r'(..)(..)(..)', rgb)[1:4] ]
-    res = []
-    for part in parts:
-        i = 0
-        while i < len(incs)-1:
-            s, b = incs[i], incs[i+1]  # smaller, bigger
-            if s <= part <= b:
-                s1 = abs(s - part)
-                b1 = abs(b - part)
-                if s1 < b1: closest = s
-                else: closest = b
-                res.append(closest)
-                break
-            i += 1
-    #print '***', res
-    res = ''.join([ ('%02.x' % i) for i in res ])
-    equiv = RGB2SHORT_DICT[ res ]
-    #print '***', res, equiv
-    return equiv, res
 
-RGB2SHORT_DICT, SHORT2RGB_DICT = _create_dicts()
+def rgb2short(rgb):
+    color_distance = float('inf')
+    color_index = -1
+
+    for i in range(256):
+        dist = compute_color_distance(rgb, CLUT[i][1])
+        if dist < color_distance:
+            color_distance = dist
+            color_index = i
+
+    return CLUT[color_index]
+
+
+def compute_color_distance(color1, color2):
+    r1, g1, b1 = break_color(color1)
+    r2, g2, b2 = break_color(color2)
+
+    distance = math.sqrt(math.pow(r1 - r2, 2) + math.pow(g1 - g2, 2) + math.pow(b1 - b2, 2))
+
+    return distance
+
+
+def break_color(color):
+    if color.startswith('#'):
+        color = color[1:]
+
+    return [ float(int(h, 16)) for h in re.split(r'(..)(..)(..)', color)[1:4] ]
+
+SHORT2RGB_DICT = dict(CLUT)
 
 #---------------------------------------------------------------------
 
