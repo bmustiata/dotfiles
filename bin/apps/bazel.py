eddc7202a49475e92624197c878fb9ef24cc873f
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2021-07-12 22:27:11 +0200

    sync

--- bin/apps/bazel.py
+++ bin/apps/bazel.py
@@ -6,7 +6,6 @@ import apps
 apps.StaticBinary(
     name="bazel",
     url="https://github.com/bazelbuild/bazel/releases/download/{version}/bazel-{version}-linux-x86_64",
-    version="3.4.1",
+    version="4.1.0",
     shortcut="/home/raptor/bin/bazel",
 )
-
