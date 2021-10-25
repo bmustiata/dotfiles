c9bf95736bdec6b63326a765f3f6ebae2b87abc3
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2021-10-25 09:20:15 +0200

    sync

--- bin/apps/bpmn-editor.py
+++ bin/apps/bpmn-editor.py
@@ -6,5 +6,5 @@ import apps
 apps.JarExecutable(
     name="yaoqiang-bpmn-editor",
     url="https://jaist.dl.sourceforge.net/project/bpmn/{version_1}-GPLv3/binaries/{version_2}/yaoqiang-bpmn-editor-{version}.jar",
-    version="5.5.1",
+    version="5.5.2",
 )
