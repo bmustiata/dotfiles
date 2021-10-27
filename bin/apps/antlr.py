9cbe3516abb7ec55190d0ad1244b327df6f37b02
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2021-10-27 12:59:43 +0200

    sync

---
+++ bin/apps/antlr.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+import apps
+
+apps.JarExecutable(
+    name="antlr-complete",
+    url="https://www.antlr.org/download/antlr-{version}-complete.jar",
+    version="4.9.2",
+)
+
