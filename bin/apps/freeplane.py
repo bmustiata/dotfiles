137b46edf4d84b5a3cf7825998397c4f46f96fc9
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2021-12-04 16:52:42 +0100

    sync

--- bin/apps/freeplane.py
+++ bin/apps/freeplane.py
@@ -6,8 +6,8 @@ import apps
 
 apps.ZippedBinary(
     name="freeplane",
-    url="https://svwh.dl.sourceforge.net/project/freeplane/freeplane%20stable/archive/{version}/freeplane_bin-{version}.zip",
-    version="1.8.6",
+    url="https://master.dl.sourceforge.net/project/freeplane/freeplane%20stable/archive/{version}/freeplane_bin-{version}.zip",
+    version="1.9.11",
     executable="freeplane-{version}/freeplane.sh",
 )
 
