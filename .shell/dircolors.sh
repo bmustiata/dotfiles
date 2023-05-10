2af764515d20e519a8e7ebed9d4497cd99135eb7
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2023-05-10 17:04:39 +0200

    sync

--- .shell/dircolors.sh
+++ .shell/dircolors.sh
@@ -15,9 +15,10 @@ update_ls_colors() {
         local missing_color="mi=38;2;255;98;98"         ##FF6262  #red4
     else # not [[ "$GESPACE_THEME" == "light" ]]
         local file_color="fi=38;2;18;18;18"             ##121212  #black
-        local executable_color="ex=38;2;0;139;0;1"      ##008B00  #green0
+        local executable_color="ex=38;2;0;139;0;1"        ##008B00  #green0
         local directory_color="di=38;2;3;77;109;1"      ##034D6D  #blue0 bold
         local link_color="ln=38;2;3;77;109;3"           ##034D6D  #blue0 italic
+        local link_color="ln=38;2;8;129;181"            ##034D6D  #blue0 italic
         local pipe_color="pi=38;2;174;92;0;3"           ##AE5C00  #brown0 italic
         local socket_color="so=38;2;174;92;0;1"         ##AE5C00  #brown0 bold
         local block_device_color="bd=38;2;222;0;0;1"    ##DE0000  #red bold
