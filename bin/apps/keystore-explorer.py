157acb69bddb8c707a48de2f6c1d28d9f424a54a
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2023-05-16 17:07:59 +0200

    sync

--- bin/apps/keystore-explorer.py
+++ bin/apps/keystore-explorer.py
@@ -7,7 +7,7 @@ import apps
 apps.ZippedBinary(
     name="keystore-explorer",
     url="https://github.com/kaikramer/keystore-explorer/releases/download/v{version}/kse-{version_major}{version_minor}{version_patch}.zip",
-    version="5.5.1",
+    version="5.5.2",
     executable="kse-{version_major}{version_minor}{version_patch}/kse.sh",
 )
 
