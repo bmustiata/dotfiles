8d848aecf14366ecb35752bb893673ae08859197
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2025-09-25 14:50:03 +0200

    sync

--- bin/ae/aake/aake-get-helm-chart.py
+++ bin/ae/aake/aake-get-helm-chart.py
@@ -12,15 +12,26 @@ import textwrap
 @click.option("--branch", "-b",
               help="Developer branch to download the helmchart for. ie. master (default)",
               default="master")
-def main(version: str, branch: str) -> None:
+@click.option("--exact", "-e", is_flag=True, default=False,
+              help="Download the exact version of the helm chart.")
+@click.option("--only-print", "--print", "-p", is_flag=True, default=False,
+              help="Only print the depman URL")
+def main(version: str, branch: str, exact: bool, only_print: bool) -> None:
+    exact_flag = ""
+    if exact:
+        exact_flag = "--exact"
+
     if version != "<none>":
         depman_download = textwrap.dedent(f"""\
-        ~/bin/ae/depman-download.py --lifecycle-entity HELM --delivery Install.HELM_Automic --version {version} -o out.zip
-        """)
+        ~/bin/ae/depman-download.py --lifecycle-entity HELM --delivery Install.HELM_Automic --version {version} -o out.zip {exact_flag}""")
     else:
         depman_download = textwrap.dedent(f"""\
-        ~/bin/ae/depman-download.py --lifecycle-entity HELM --delivery Install.HELM_Automic --branch {branch} -o out.zip
-        """)
+        ~/bin/ae/depman-download.py --lifecycle-entity HELM --delivery Install.HELM_Automic --branch {branch} -o out.zip {exact_flag}""")
+
+    if only_print:
+        depman_download = f"{depman_download} --print"
+        subprocess.check_call(["/bin/sh", "-c", depman_download])
+        return
 
     command = textwrap.dedent(f"""\
       set -e
