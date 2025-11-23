66b62ba4b396f50c246a734243923b91bcffb8e5
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2025-11-23 22:15:49 +0100

    sync

--- bin/docker/hercules.sh
+++ bin/docker/hercules.sh
@@ -1,10 +1,12 @@
 
 echo "console on 3270, web on 8038."
+echo "admin: HERC01 , CUL8TR"
+echo " user: HERC02 , CUL8TR"
+echo "container will run without --rm, remove it manually"
 
 docker run -it \
-    --rm \
     --name hercules \
     -p 3270:3270 \
     -p 8038:8038 \
-    rattydave/docker-ubuntu-hercules-mvs:latest
+    skunklabs/tk5-hercules
 
