c707625dedb98ffc945cee0dbad40e91f85fe695
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2026-03-13 05:44:23 +0100

    sync

--- bin/ge_ai_util.py
+++ bin/ge_ai_util.py
@@ -9,7 +9,7 @@ from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 
 # Set the URL of your vLLM server
-inference_server_url = "http://localhost:8000/v1"
+inference_server_url = "http://gmktek:11434/v1"
 #model_name="Qwen/Qwen2.5-14B-Instruct-1M"
 #model_name="Qwen/Qwen2.5-7B-Instruct"
 model_name="gpt-4"
